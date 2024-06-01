8fb31d53aaaf54031d23834b106522e8a9a35bcb
ImoBassey000 <ibassey000@gmail.com>
2024-06-01 08:30:58 +0100

    updates

--- database_utils.py
+++ database_utils.py
@@ -7,12 +7,16 @@ class DatabaseConnector:
     def read_db_creds(self):
         with open("db_creds.yaml", "r") as file:
          return yaml.safe_load(file)
-
+        
     def init_db_engine(self):
         db_creds = self.read_db_creds()
         engine_str = f"postgresql+psycopg2://{db_creds['RDS_USER']}:{db_creds['RDS_PASSWORD']}@{db_creds['RDS_HOST']}:{db_creds['RDS_PORT']}/{db_creds['RDS_DATABASE']}"
         return create_engine(engine_str)
     
+    def read_local_creds(self):
+       with open("local_cred.yaml", "r") as file_2:
+        return yaml.safe_load(file_2)
+
     def init_local_engine(self):
         local_creds = self.read_local_creds()
         engine_2 = f"postgresql+psycopg2://{local_cred['username']}:{local_cred['password']}@{local_cred['host']}:{local_creds['port']}/{local_cred['database']}"
--- main.py
+++ main.py
@@ -9,14 +9,10 @@ with open("db_creds.yaml", "r") as file:
 with open("local_cred.yaml", "r") as file_2:
     local_konet = yaml.safe_load(file_2)
 
-    def init_local_engine(self):
-        local_creds = self.read_local_creds()
-        engine_2 = f"postgresql+psycopg2://{local_cred['username']}:{local_cred['password']}@{local_cred['host']}:{local_creds['port']}/{local_cred['database']}"
-        return create_engine(engine_2)
 
 data_cleaner = DataCleaning(db_creds)
 legacy_users_cleaned = data_cleaner.clean_user_data()
 
 db_connector = DatabaseConnector()
-db_connector.upload_to_db(legacy_users_cleaned, "Sales['dim_users']")
+db_connector.upload_to_db(legacy_users_cleaned, "sales_data['dim_users']")
 
