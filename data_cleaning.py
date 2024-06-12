e2d723ff9a92da78d763b5c926877559f1d68126
ImoBassey000 <ibassey000@gmail.com>
2024-06-12 16:58:42 +0100

    update to clean_store_data in datacleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -35,11 +35,12 @@ class DataCleaning:
 #  Ensure correct data types
         stores_df['staff_numbers'] = pd.to_numeric(stores_df['staff_numbers'], errors='coerce')
         stores_df['opening_date'] = pd.to_datetime(stores_df['opening_date'], errors='coerce')
+# Replace newline character in the 'address' column with an underscore
+        stores_df['address'] = stores_df['address'].str.replace("\n", "_")
 #  Drop rows with all NaNs in critical columns
         stores_df = stores_df.dropna(subset=['address', 'longitude', 'latitude', 'locality', 'store_code', 'staff_numbers', 'opening_date', 'store_type', 'country_code', 'continent'])
         return stores_df
     
-
 
     def convert_product_weights(self, products_df):
         def convert_weight(value):
